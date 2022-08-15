9aedb45d30f0066871651b85264787f08c836416
clebemachado <clebmachado.em@gmail.com>
2022-08-15 19:06:24 -0300

    Update vagas controller

--- src/main/java/com/dbc/curriculo/controller/VagaController.java
+++ src/main/java/com/dbc/curriculo/controller/VagaController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,10 @@ public class VagaController {
 
     private final VagaService vagaService;
 
-    @GetMapping
-    public ResponseEntity<VagaApiDTO> getVagas(Integer pagina, Integer quantidade){
+    @GetMapping("/pagina={pagina}/quantidade={quantidade}")
+    public ResponseEntity<VagaApiDTO> getVagas(
+            @PathVariable("pagina") Integer pagina,
+            @PathVariable("quantidade") Integer quantidade){
         return ResponseEntity.ok(vagaService.getVagas(pagina, quantidade));
     }
 
